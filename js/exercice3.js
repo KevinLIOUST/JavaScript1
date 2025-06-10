ab3e846b762094ad5b4c7478f9a6aae7ec4a52cb
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-10 15:54:50 +0200

    Exercice 6 Terminé ! :)

--- js/exercice3.js
+++ js/exercice3.js
@@ -4,3 +4,4 @@ let city = "Le Havre";
 
 alert("Je m'appelle " + firstName + ", j'ai " + age + " an(s), et j'habite à " + city + " !");
 alert("Je m'appelle ".concat(firstName).concat(", j'ai ").concat(age).concat(" an(s), et j'habite à ").concat(city).concat(" !!!!!!!"));
+alert(`Je m'appelle ${firstName}, j'ai ${age} an(s), et j'habite à ${city} !!!`);
