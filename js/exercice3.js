b4aa426b1aac668041c096a501d27abf909212ab
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-10 13:48:07 +0200

    Exercice 3 Terminé ! :)

---
+++ js/exercice3.js
@@ -0,0 +1,6 @@
+let firstName = "Kevin";
+let age = 24;
+let city = "Le Havre";
+
+alert("Je m'appelle " + firstName + ", j'ai " + age + " an(s), et j'habite à " + city + " !");
+alert("Je m'appelle ".concat(firstName).concat(", j'ai ").concat(age).concat(" an(s), et j'habite à ").concat(city).concat(" !!!!!!!"));
