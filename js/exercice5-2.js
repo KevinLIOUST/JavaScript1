d6d4638da6437dad4d030261cef684930a6d4ed4
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-10 14:30:49 +0200

    Exercice 5 Terminé avec amélioration ! :)

---
+++ js/exercice5-2.js
@@ -0,0 +1,4 @@
+let nb1 = Number(prompt("Veuillez entrer un nombre pour le 1 :"));
+let nb2 = Number(prompt("Veuillez entrer un nombre pour le 2 :"));
+
+alert("Le résultat de l'addition est : " + nb1 + " + " + nb2 + " = " + Number(nb1 + nb2));
