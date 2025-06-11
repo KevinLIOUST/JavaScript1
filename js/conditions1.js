ca48b6c5bba3a6d26de062e87534c3569643ade9
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-11 08:58:04 +0200

    conditions1.js terminé ! :)

---
+++ js/conditions1.js
@@ -0,0 +1,7 @@
+let age = prompt("Quel est votre age ?");
+
+if(age >= 18) {
+    alert("C'est bon, vous êtes adulte ! :)");
+} else {
+    alert("C'est pas bon, vous êtes pas encore adulte ! ):");
+}
