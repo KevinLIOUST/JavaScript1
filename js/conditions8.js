3c0d3e581b7358d1f3692521974655d829397fb2
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-11 13:15:53 +0200

    conditions8.js terminé ! :)

---
+++ js/conditions8.js
@@ -0,0 +1,5 @@
+if (window.confirm("En cliquant sur OK, je confirme que je rentre sur ce site en 'mon âme et conscience'")) {
+    alert("OK !!!! :) :) :) :)");
+} else {
+    alert("Pas validé !!!! ): ): ): ):");
+}
