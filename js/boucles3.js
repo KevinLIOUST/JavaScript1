b0ea11026a857d6392bedded5f7cd06ed4f695b6
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-11 16:54:08 +0200

    boucles3.js terminé et boucles4.js commencé ! :)

--- js/boucles3.js
+++ js/boucles3.js
@@ -3,5 +3,6 @@ let nb2 = 6;
 
 while (Number(nb1) + Number(nb2) < 2025) {
     console.log(Number(nb1) + Number(nb2));
+    nb1 = Number(nb2);
     nb2 = Number(nb1) + Number(nb2);
 }
---
+++ js/boucles4.js
@@ -0,0 +1,8 @@
+let i = 1;
+
+while (i <= 10) {
+    console.log("Message " + i);
+    if (i == 10) {
+        console.log("Message " + 10 + ", C'est le dernier message !");
+    }
+}
