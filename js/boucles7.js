53b162c89e2749042fbefba81373c489f0f86677
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-11 17:21:16 +0200

    boucles7.js continue ! :)

---
+++ js/boucles7.js
@@ -0,0 +1,9 @@
+for (let i = 20; i > 0; i--) {
+    if (i == 20) {
+        console.log("Start !!");
+    } else if (i < 0) {
+        console.log(i);
+    } else {
+        console.log("Finish !!");
+    }
+}
