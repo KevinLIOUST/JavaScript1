02c9ecae771c9e9b39e3a611a6a22b15daf3bc99
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-11 09:57:42 +0200

    conditions2.js terminé ! :)

---
+++ js/conditions2.js
@@ -0,0 +1,15 @@
+let nb = +prompt("Veuillez entrer un nombre entier :");
+
+// je teste si c'est different de NaN
+if (!isNaN(nb)) {
+    alert("C'est bon ! :) C'est bien un nombre entier ! :)");
+
+    if (nb % 2 == 0) {
+        alert("C'est un nombre pair ! :)");
+    } else {
+        alert("C'est un nombre impair ! :)");
+    }
+
+} else {
+    alert("Vous devez entrer un nombre entier pour que ça marche. :)");
+}
