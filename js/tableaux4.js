d9bb1ca3aff7ebce5b469c9385ed1dffc508bd7f
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-12 15:47:10 +0200

    tableaux4.js Terminé ! :)

---
+++ js/tableaux4.js
@@ -0,0 +1,5 @@
+let tabMois = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+
+for (let i = 0; i < tabMois.length; i++) {
+    console.log(i + 1 + " - " + tabMois[i]);
+}
