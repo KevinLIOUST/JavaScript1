384459572981ef78e808c29c6f4901d751da6994
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-12 15:19:45 +0200

    Guess-my-number-1.js et tableau1.js Terminés ! :)

---
+++ js/tableaux1.js
@@ -0,0 +1,18 @@
+let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+let tabMois2 = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+
+// for (let i = 0; i < tabMois.length; i++) {
+//     console.log(tabMois[i]);
+// }
+
+// for (let i = 0; i < tabMois2.length; i++) {
+//     console.log(tabMois2[i]);
+// }
+
+// tabMois.forEach((mois) => {
+//     console.log(mois);
+// });
+
+// tabMois2.forEach((mois) => {
+//     console.log(mois);
+// });
