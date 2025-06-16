d1b65ce3cf7842ba51e1e1ff368ac297e1603ab0
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-16 11:51:26 +0200

    tableaux5.js avec amélioration terminé ! :)

--- js/tableaux5.js
+++ js/tableaux5.js
@@ -5,11 +5,18 @@ let tabJourSemaineFr = new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendred
 let tabJourSemaineEn = new Array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 
 // C'est un tableau a 2 dimensions, ou un tableau de tableaux LOL !!!! :) :) :) :)
+// let tabCalendrier = [
+//     tabMoisFr,
+//     tabJourSemaineFr,
+//     tabMoisEn,
+//     tabJourSemaineEn
+// ]
+
+// Amélioration
+
 let tabCalendrier = [
-    tabMoisFr,
-    tabJourSemaineFr,
-    tabMoisEn,
-    tabJourSemaineEn
+    [tabMoisFr, tabMoisEn],
+    [tabJourSemaineFr, tabJourSemaineEn]
 ]
 
 // nomTableau[][] : les premiers doubles crochets servent à sélectionner le tableau sur lequel on souhaite travailler à partir de l'indice 0.
@@ -17,17 +24,17 @@ let tabCalendrier = [
 // Donc, le 0 correspond au 1° élément, le 1 au 2°, le 2 au 3°, etc.
 
 // Affichage de Novembre
-console.log(tabCalendrier[0][10]);
+console.log(tabCalendrier[0][0][10]);
 
 // Affichage de Friday
-console.log(tabCalendrier[3][4]);
+console.log(tabCalendrier[1][1][4]);
 
 // Affichage de tous les mois en Anglais
-for (let i = 0; i < tabCalendrier[2].length; i++) {
-    console.log(tabCalendrier[2][i]);
+for (let i = 0; i < tabCalendrier[0][1].length; i++) {
+    console.log(tabCalendrier[0][1][i]);
 }
 
 // Affichage de tous les jours de la semaine en Français
-for (let i = 0; i < tabCalendrier[1].length; i++) {
-    console.log(tabCalendrier[1][i]);
+for (let i = 0; i < tabCalendrier[1][0].length; i++) {
+    console.log(tabCalendrier[1][0][i]);
 }
