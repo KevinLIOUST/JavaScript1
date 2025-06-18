f1a3fe4a03c68348dbec6bac17762120dd8e4cea
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-18 09:34:19 +0200

    fonctions-level1.js et fonctions-level2.js Terminés ! :)

---
+++ js/fonctions-level-2.js
@@ -0,0 +1,9 @@
+function saluerNom(nom) {
+    return `Bonjour ${nom} ! :)`;
+}
+
+console.log(saluerNom("Kevin"));
+console.log(saluerNom("Jeanne d'Arc"));
+console.log(saluerNom("Lucy"));
+console.log(saluerNom("Wendy"));
+console.log(saluerNom("Carla"));
